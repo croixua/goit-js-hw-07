a542fa5c288259301a7d13d197993f48c9438ef3
croixua <croixua@gmail.com>
2022-01-18 21:35:26 +0200

    create markup and add to html

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const itemsGallaryMarkup = createGallaryItemsMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', itemsGallaryMarkup);
+
+galleryRef.addEventListener('click', onImageClick);
+
+function createGallaryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview: previewLinkImg, original: originalLinkImg, description }) => {
+        return `
+        <a class="gallery__item" href="${originalLinkImg}">
+            <img class="gallery__image" src="${previewLinkImg}" alt="${description}" />
+        </a>
+          `;
+      },
+    )
+    .join('');
+}
+
+function onImageClick(e) {
+  e.preventDefault();
+}
