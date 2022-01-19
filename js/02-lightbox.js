09b7ec8f4590b0ae9a3a136307103ac345212e26
croixua <croixua@gmail.com>
2022-01-19 21:43:15 +0200

    refactoring task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,30 +6,21 @@ const itemsGallaryMarkup = createGallaryItemsMarkup(galleryItems);
 
 galleryRef.insertAdjacentHTML('beforeend', itemsGallaryMarkup);
 
-galleryRef.addEventListener('click', openLightbox);
-
 function createGallaryItemsMarkup(galleryItems) {
   return galleryItems
     .map(
       ({ preview: previewLinkImg, original: originalLinkImg, description }) => {
         return `
-        <a class="gallery__item" href="${originalLinkImg}">
+            <a class="gallery__item" href="${originalLinkImg}">
             <img class="gallery__image" src="${previewLinkImg}" alt="${description}" />
-        </a>
-          `;
+            </a>
+            `;
       },
     )
     .join('');
 }
 
-function openLightbox(e) {
-  e.preventDefault();
-
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-    captionsData: 'alt',
-  });
-  console.log(lightbox);
-
-  lightbox.open();
-}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
